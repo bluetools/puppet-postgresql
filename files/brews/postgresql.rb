8539766d5ba9712c2c9ae601d5568ca9ec519a2f
Don Pinkster <don@moneybird.com>
2013-07-04 11:49:04 +0200

    fix

--- files/brews/postgresql.rb
+++ files/brews/postgresql.rb
@@ -3,107 +3,57 @@ require 'formula'
 class Postgresql < Formula
   homepage 'http://www.postgresql.org/'
   url 'http://ftp.postgresql.org/pub/source/v9.3beta2/postgresql-9.3beta2.tar.bz2'
+  version '9.3beta2'
   sha256 '67b4f1e8be622ab2f32b2a7823b3185052c93eae11c961214d87be76978f35ae'
-  version '9.3beta2-boxen1'
+  head 'http://git.postgresql.org/git/postgresql.git', :branch => 'REL9_3_STABLE'
 
-  depends_on 'readline'
+  #keg_only 'The different provided versions of PostgreSQL conflict with each other.'
+
+  #env :std
+
+  depends_on 'gettext'
   depends_on 'ossp-uuid'
+  depends_on 'readline'
 
-  def options
-    [
-      ['--32-bit', 'Build 32-bit only.'],
-      ['--no-python', 'Build without Python support.'],
-      ['--no-perl', 'Build without Perl support.'],
-      ['--enable-dtrace', 'Build with DTrace support.']
-    ]
-  end
-
-  # Fix PL/Python build: https://github.com/mxcl/homebrew/issues/11162
   # Fix uuid-ossp build issues: http://archives.postgresql.org/pgsql-general/2012-07/msg00654.php
   def patches
     DATA
   end
 
-  skip_clean :all
-
-  fails_with :clang do
-    build 211
-    cause 'Miscompilation resulting in segfault on queries'
-  end
-
   def install
-    ENV.libxml2 if MacOS.snow_leopard?
-
-    args = [
-      "--disable-debug",
-      "--prefix=#{prefix}",
-      "--datadir=#{share}/#{name}",
-      "--docdir=#{doc}",
-      "--enable-thread-safety",
-      "--with-bonjour",
-      "--with-gssapi",
-      "--with-krb5",
-      "--with-openssl",
-      "--with-libxml",
-      "--with-libxslt",
-      "--with-libedit"
-    ]
-
-    args << "--with-ossp-uuid" unless ARGV.include? '--no-ossp-uuid'
-    args << "--with-python" unless ARGV.include? '--no-python'
-    args << "--with-perl" unless ARGV.include? '--no-perl'
-    args << "--enable-dtrace" if ARGV.include? '--enable-dtrace'
-
-    ENV.append 'CFLAGS', `uuid-config --cflags`.strip
-    ENV.append 'LDFLAGS', `uuid-config --ldflags`.strip
-    ENV.append 'LIBS', `uuid-config --libs`.strip
-
-    if not ARGV.build_32_bit? and MacOS.prefer_64_bit? and not ARGV.include? '--no-python'
-      args << "ARCHFLAGS='-arch x86_64'"
-      check_python_arch
-    end
-
-    if ARGV.build_32_bit?
-      ENV.append 'CFLAGS', '-arch i386'
-      ENV.append 'LDFLAGS', '-arch i386'
-    end
-
-    # Fails on Core Duo with O4 and O3
-    ENV.O2 if Hardware.intel_family == :core
+    args = ["--prefix=#{prefix}",
+            "--enable-dtrace",
+            "--enable-nls",
+            "--with-bonjour",
+            "--with-gssapi",
+            "--with-krb5",
+            "--with-ldap",
+            "--with-libxml",
+            "--with-libxslt",
+            "--with-openssl",
+            "--with-ossp-uuid",
+            "--with-pam",
+            "--with-perl",
+            "--with-python",
+            "--with-tcl"]
 
     system "./configure", *args
-    system "make install-world"
-  end
-
-  def check_python_arch
-    # On 64-bit systems, we need to look for a 32-bit Framework Python.
-    # The configure script prefers this Python version, and if it doesn't
-    # have 64-bit support then linking will fail.
-    framework_python = Pathname.new "/Library/Frameworks/Python.framework/Versions/Current/Python"
-    return unless framework_python.exist?
-    unless (archs_for_command framework_python).include? :x86_64
-      opoo "Detected a framework Python that does not have 64-bit support in:"
-      puts <<-EOS.undent
-          #{framework_python}
-
-        The configure script seems to prefer this version of Python over any others,
-        so you may experience linker problems as described in:
-          http://osdir.com/ml/pgsql-general/2009-09/msg00160.html
-
-        To fix this issue, you may need to either delete the version of Python
-        shown above, or move it out of the way before brewing PostgreSQL.
-
-        Note that a framework Python in /Library/Frameworks/Python.framework is
-        the "MacPython" version, and not the system-provided version which is in:
-          /System/Library/Frameworks/Python.framework
-      EOS
+    if build.head?
+      # XXX Can't build docs using Homebrew-provided software, so skip
+      # it when building from Git.
+      system "make install"
+      system "make -C contrib install"
+    else
+      system "make install-world"
     end
   end
+
 end
 
+
 __END__
---- a/contrib/uuid-ossp/uuid-ossp.c 2012-07-30 18:34:53.000000000 -0700
-+++ b/contrib/uuid-ossp/uuid-ossp.c 2012-07-30 18:35:03.000000000 -0700
+--- a/contrib/uuid-ossp/uuid-ossp.c     2012-07-30 18:34:53.000000000 -0700
++++ b/contrib/uuid-ossp/uuid-ossp.c     2012-07-30 18:35:03.000000000 -0700
 @@ -9,6 +9,8 @@
   *-------------------------------------------------------------------------
   */
