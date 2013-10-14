8e60e123c68045be7c9b4af893715ac53f4e48cc
Marcus Crafter <crafterm@redartisan.com>
2013-10-15 10:57:16 +1100

    [NEW] Update to 9.3.1

--- files/brews/postgresql.rb
+++ files/brews/postgresql.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Postgresql < Formula
   homepage 'http://www.postgresql.org/'
-  url 'http://ftp.postgresql.org/pub/source/v9.3.0/postgresql-9.3.0.tar.bz2'
-  sha256 '784e5db7d8fd69b08ddb125c4452211e04b089516359a5cf72d292cade7c728e'
-  version '9.3.0-boxen1'
+  url 'http://ftp.postgresql.org/pub/source/v9.3.1/postgresql-9.3.1.tar.bz2'
+  sha256 '8ea4a7a92a6f5a79359b02e683ace335c5eb45dffe7f8a681a9ce82470a8a0b8'
+  version '9.3.1-boxen1'
 
   #keg_only 'The different provided versions of PostgreSQL conflict with each other.'
 
